f7f55e1129628e94f9b5ce7b8d0d1ad8243c8ce0
decjonas <decjonas@proton.me>
2024-05-14 15:16:43 +0200

    fix

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { Person, Search } from "svelte-ionicons";
+  import { CircleUserRound, Search } from "lucide-svelte";
 
   import ListView from "$lib/list/ListView.svelte";
   import ListItem from "$lib/list/ListItem.svelte";
@@ -9,12 +9,12 @@
 
 <section class="grid grid-cols-1 gap-4 p-4">
   <search
-    class="flex bg-[var(--base)] items-center pl-4 gap-1.5 rounded-md text-[var(--gray-1)]"
+    class="flex bg-[var(--base)] text-[var(--gray-1)] items-center pl-4 gap-2 rounded-md"
   >
     <Search size="20" />
     <input
       type="text"
-      class="outline-none h-[44px] placeholder:text-inherit grow"
+      class="outline-none h-[44px] text-[var(--text)] bg-transparent placeholder:text-[var(--gray-1)] grow"
       placeholder="Search Posts, Subreddits, Users"
       bind:value={query}
     />
@@ -27,7 +27,7 @@
         Go to Subreddit "{query}"
       </ListItem>
       <ListItem href="u/{query}" class="flex items-center gap-2">
-        <Person size="20" class="text-[var(--gray-1)]" />
+        <CircleUserRound size="20" class="text-[var(--gray-1)]" />
         Go to User "{query}"
       </ListItem>
     </ListView>
